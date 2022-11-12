9345ade8a8f222fdf7b69de8b4040c448cd92b2a
Leonardo Metzger <leonardo.metzger@outlook.com>
2022-11-12 08:51:47 -0300

    chore: update the pathing test

--- pkg/util/path_test.go
+++ pkg/util/path_test.go
@@ -1,6 +1,7 @@
 package utils_test
 
 import (
+	"regexp"
 	"testing"
 
 	utils "github.com/leometzger/mmonitoring/pkg/util"
@@ -14,19 +15,19 @@ func TestKeyPathing(t *testing.T) {
 	}{
 		{
 			url:      "https://google.com",
-			expected: "reports/google.com/2022/10/10/11/30/id.json",
+			expected: "reports\\/google.com\\/20[0-9]{2}\\/(0|1)[0-9]\\/[0-3][0-9]\\/[0-2][0-9]\\/[a-z0-9]{40}\\.json$",
 		},
 		{
 			url:      "https://google.com/search?q=breaking+bad",
-			expected: "reports/google.com/2022/02/10/23/30/id.json",
+			expected: "reports\\/google.com\\/20[0-9]{2}\\/(0|1)[0-9]\\/[0-3][0-9]\\/[0-2][0-9]\\/[a-z0-9]{40}\\.json$",
 		},
 		{
 			url:      "https://facebook.com",
-			expected: "reports/facebook.com/2022/10/10/11/30/id.json",
+			expected: "reports\\/facebook.com\\/20[0-9]{2}\\/(0|1)[0-9]\\/[0-3][0-9]\\/[0-2][0-9]\\/[a-z0-9]{40}\\.json$",
 		},
 	}
 
 	for _, test := range cases {
-		assert.Equal(t, test.expected, utils.GetPathFromUrl(test.url))
+		assert.Regexp(t, regexp.MustCompile(test.expected), utils.GetPathFromUrl(test.url))
 	}
 }
